2bec09312d206a9b1c805317659efa2d48f2bc54
Datseris <datseris.george@gmail.com>
2022-02-22 13:52:48 +0100

    combine era5 files

--- downloads/combine_era5.jl
+++ downloads/combine_era5.jl
@@ -2,12 +2,14 @@
 # into a single file with all the years. It is more convenient to work with data this way.
 
 using DrWatson
-@quickactivate ""
+@quickactivate "MinimallyFittingCloudiness"
 using ClimateBase, NCDatasets, Glob
 
 era5dir = desktop("ERA5_original")
 cd(era5dir)
 
-era5 = glob("ERA5_monthly_2D_reanalysis*")
-alldata = NCDataset(era5; aggdim = "time")
-write("ERA5_2D.nc", alldata)
+for f in ("ERA5_monthly_3D", "ERA5_monthly_2D")
+    era5 = glob(f*"*")
+    alldata = NCDataset(era5; aggdim = "time")
+    write(f*".nc", alldata)
+end
